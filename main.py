63a0a7b5bf7a56121b8489e051efd97b854a88c3
Shahmeer0311 <22alis@students.watfordboys.org>
2024-11-17 14:20:10 +0000

    Data types

---
+++ main.py
@@ -0,0 +1,8 @@
+a = 5
+print("Data Types of A" , type(a))
+b = 0.7
+print("Data Types of A" , type(b))
+c = True
+print("Data Types of A" , type(c))
+d = "string"
+print("Data Types of A" , type(d))
